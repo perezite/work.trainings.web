4e333616db02492884ad7609fb5adef237b955f3
Prez <simulo2000@gmail.com>
2023-02-05 13:48:22 +0100

    mobile layout wip

---
+++ Sidebar/Experiment2/index-script.js
@@ -0,0 +1,9 @@
+const sideNavToggle = document.querySelector('.side-nav-toggle');
+const sideNav = document.querySelector('.side-nav');
+const mainContent = document.querySelector('.main-content');
+sideNavToggle.addEventListener('click', () => {
+    sideNav.classList.add('toggled')
+});
+mainContent.addEventListener('click', () => {
+    sideNav.classList.remove('toggled');
+});
