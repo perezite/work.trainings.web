a635bdfac10dabeacb5489644129b7b3b52a17a7
Prez <simulo2000@gmail.com>
2023-02-09 23:09:03 +0100

    experimenting wip

--- Dashboard/Dashboard1/scripts.js
+++ Dashboard/Dashboard1/scripts.js
@@ -5,10 +5,12 @@ const mainContent = document.querySelector('.main-content');
 
 sideNavToggle.addEventListener('click', () => {
     sideNav.classList.toggle('toggled')
+    document.body.classList.add('navbar-expanded');
 });
 
 sideNavCloseToggle.addEventListener('click', () => {
     sideNav.classList.remove('toggled');
+    document.body.classList.remove('navbar-expanded');
     console.log('untoggle');
 });
 
