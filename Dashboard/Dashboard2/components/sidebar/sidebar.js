627b4e3e78984ad226a60759eec2e91ad02e028c
Prez <simulo2000@gmail.com>
2023-02-12 21:44:21 +0100

    sidebar wip

--- Dashboard/Dashboard2/components/sidebar/sidebar.js
+++ Dashboard/Dashboard2/components/sidebar/sidebar.js
@@ -1,4 +1,8 @@
 const sidebar = document.querySelector('.sidebar');
 const sidebarToggle = document.querySelector('.sidebar-toggle');
+const sidebarCloser = document.querySelector('.sidebar-closer');
 
-sidebarToggle.addEventListener('click', () => sidebar.classList.toggle('sidebar-expanded'));
+const sidebarExpandedClass = 'sidebar-expanded';
+
+// sidebarToggle.addEventListener('click', () => sidebar.classList.toggle(sidebarExpandedClass));
+// sidebarCloser.addEventListener('click', () => sidebar.classList.remove(sidebarExpandedClass));
