4eceec0ecfe4500b102a2d921b4477dac574c200
Prez <simulo2000@gmail.com>
2023-02-19 17:01:22 +0100

    integrate sidebar in main page wip

--- Dashboard/Dashboard2/components/sidebar/sidebar.js
+++ Dashboard/Dashboard2/components/sidebar/sidebar.js
@@ -4,5 +4,8 @@ const sidebarCloser = document.querySelector('.sidebar-closer');
 
 const sidebarExpandedClass = 'sidebar-expanded';
 
-sidebarToggle.addEventListener('click', () => sidebar.classList.toggle(sidebarExpandedClass));
-sidebarCloser.addEventListener('click', () => sidebar.classList.remove(sidebarExpandedClass));
+// sidebarToggle.addEventListener('click', () => sidebar.classList.toggle(sidebarExpandedClass));
+// sidebarCloser.addEventListener('click', () => sidebar.classList.remove(sidebarExpandedClass));
+
+sidebarToggle.addEventListener('click', () => document.body.classList.toggle(sidebarExpandedClass));
+sidebarCloser.addEventListener('click', () => document.body.classList.remove(sidebarExpandedClass));
---
+++ Dashboard/Dashboard2/index.js
@@ -0,0 +1,10 @@
+// const navbarToggle = document.querySelector('.navbar-toggle');
+// const sidebar = document.querySelector('.sidebar');
+// const sidebarCloser = document.querySelector('.sidebar-closer');
+
+// const sidebarExpandedClass = 'sidebar-expanded';
+
+// console.log('test');
+
+// navbarToggle.addEventListener('click', () => sidebar.classList.toggle(sidebarExpandedClass));
+// sidebarCloser.addEventListener('click', () => sidebar.classList.remove(sidebarExpandedClass));
