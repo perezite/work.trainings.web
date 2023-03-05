056f2bc225887166d23ab516516f6576b102f783
Prez <simulo2000@gmail.com>
2023-03-05 18:42:44 +0100

    close sidebar when main element is clicked on mobile screens

--- Dashboard/Dashboard2/index.js
+++ Dashboard/Dashboard2/index.js
@@ -1,13 +1,19 @@
 const navbarToggle = document.querySelector('.navbar-toggle');
 const sidebarCloser = document.querySelector('.sidebar-closer');
+const main = document.querySelector('main');
 
 const sidebarExpandedClass = 'sidebar-expanded';
 
+const isMobile = () => window.matchMedia("screen and (max-width: 576px)").matches;
+
 navbarToggle.addEventListener('click', () => document.body.classList.toggle(sidebarExpandedClass));
 sidebarCloser.addEventListener('click', () => document.body.classList.remove(sidebarExpandedClass));
+main.addEventListener('click', () => {
+    if (isMobile())
+        document.body.classList.remove(sidebarExpandedClass);
+});
 
 window.onload = () => {
-    const isNonMobile = window.matchMedia("screen and (min-width: 576px)").matches;
-    if (isNonMobile)
+    if (!isMobile())
         document.body.classList.add(sidebarExpandedClass);
 }
