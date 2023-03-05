346739c920de6fc3d78ce114bc699920c63ec2d1
Prez <simulo2000@gmail.com>
2023-03-05 18:10:35 +0100

    expand sidebar on load when screen is non-mobile

--- Dashboard/Dashboard2/components/sidebar/sidebar.js
+++ Dashboard/Dashboard2/components/sidebar/sidebar.js
@@ -4,8 +4,11 @@ const sidebarCloser = document.querySelector('.sidebar-closer');
 
 const sidebarExpandedClass = 'sidebar-expanded';
 
-// sidebarToggle.addEventListener('click', () => sidebar.classList.toggle(sidebarExpandedClass));
-// sidebarCloser.addEventListener('click', () => sidebar.classList.remove(sidebarExpandedClass));
-
 sidebarToggle.addEventListener('click', () => document.body.classList.toggle(sidebarExpandedClass));
 sidebarCloser.addEventListener('click', () => document.body.classList.remove(sidebarExpandedClass));
+
+window.onload = () => {
+    const isNonMobile = window.matchMedia("screen and (min-width: 576px)").matches;
+    if (isNonMobile)
+        document.body.classList.add(sidebarExpandedClass);
+}
